e4d45fc7fc296a5d11680c73bcb0444af652a056
Arieh <arieh.glazer@gmail.com>
2010-01-31 09:20:39 +0200

    JSLint fixes

--- Source/mooie6update.js
+++ Source/mooie6update.js
@@ -1,3 +1,20 @@
+/*
+---
+description: This class is a simple port to the JQuery plugin ie6-update (http://ie6update.com)
+
+license: MIT-style
+
+authors:
+- Arieh Glazer
+
+requires:
+- core/1.2.4: [Class,Class.Extras,Element.Dimensions]
+
+provides: [HashListener]
+
+...
+*/
+
 var IE6Update = new Class({
 	Implements : [Options],
 	containerHTML : "<div class='icon'><img class='normal' src=''/></div><div class='close'><img class='normal' src=''/></div><div class='content'><div>Internet Explorer is missing updates required to view this site. Click here to update... </div></div>",
@@ -11,7 +28,6 @@ var IE6Update = new Class({
 		var icon = this.container.getElements('.icon').getElements('img')[0],
 			close = this.container.getElements('.close').getElements('img')[0],
 			container=this.container,
-			containerHeight = this.container.getSize().y,
 			img_folder = this.options.img_folder;
 		
 		icon.set('src',this.options.img_folder+'icon.png');
@@ -21,19 +37,21 @@ var IE6Update = new Class({
 				container.destroy();
 				e.stopPropagation();
 			});
-		this.container
-			.addEvent('mouseover', function(){
+			
+		this.container.addEvents({
+			'mouseover': function(){
 				icon.set('src',img_folder+'icon-over.png');
 				close.set('src',img_folder+'close-over.png');
 				this.setStyle('background-color','#3399ff');
-			})
-			.addEvent('mouseout',function(){
+			},
+			'mouseout':function(){
 				icon.set('src',img_folder+'icon.png');
 				close.set('src',img_folder+'close.png');
 				this.setStyle('background-color','#ffffe1');
-			})
-			.addEvent('click',function(){window.location.href = 'http://www.microsoft.com/windows/internet-explorer/default.aspx';})
-			.setStyles({'overflow':'hidden','height':0});
+			},
+			'click':function(){window.location.href = 'http://www.microsoft.com/windows/internet-explorer/default.aspx';}
+		})
+		.setStyles({'overflow':'hidden','height':0});
 			
 		$$('body')[0].adopt(this.container);
 		this.container.tween('height',16);
