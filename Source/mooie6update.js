54d1b11d50bb0e0ecccad7e7c974d8ff19cb4b47
Arieh <arieh.glazer@gmail.com>
2010-02-01 08:43:29 +0200

    some more refactoring. added the show option (set to false by default)

--- Source/mooie6update.js
+++ Source/mooie6update.js
@@ -20,12 +20,13 @@ var IENotifier = new Class({
 	options :{
 		img_folder : 'images/',
 		text : '',
-		url : '#'
+		url : '#',
+		show : false
 	},
 	initialize : function(options){
 		this.setOptions(options);
 	
-		this.container = new Element('a',{id:'activebar-container',href:this.options.url}).set('html',this.containerHTML);
+		this.container = new Element('a',{id:'activebar-container', href:this.options.url}).set('html',this.containerHTML);
 		this.container.getElement('p').appendText(this.options.text);
 		
 		var icon = this.container.getElements('.icon'),
@@ -38,13 +39,13 @@ var IENotifier = new Class({
 		close.setStyle('background-image','url('+this.options.img_folder+'sprites.png)');
 		
 		close.addEvent('click',function(e){
-			this.hide();
+			self.hide();
 			e.stop();
 		});
 			
 		this.container.inject(document.body);
 		
-		this.show();
+		if (this.options.show) this.show();
 	},
 	toElement : function(){ return this.container; },
 	show : function(){
@@ -60,6 +61,7 @@ var IENotifier = new Class({
 function createIE6UpdateBanner(options){
 	options = options || {};
 	options.text = options.text || "Internet Explorer is missing updates required to view this site. Click here to update... ";
-	options.url = options.url || "http://www.microsoft.com/windows/internet-explorer/default.aspx";
+	options.url  = options.url  || "http://www.microsoft.com/windows/internet-explorer/default.aspx";
+	options.show = options.show || true;
 	return  new IENotifier(options || {});
 }
